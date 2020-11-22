160c9bab8f642316d63dc9025e7df702bd5d1592
Matan Ben Nagar <Matannagar@gmail.com>
2020-11-22 19:56:26 +0200

    merge edge

--- src/api/DWGraph_DS.java
+++ src/api/DWGraph_DS.java
@@ -213,4 +213,54 @@ public class DWGraph_DS implements directed_weighted_graph {
             }
         }
     }
+    private class Edge_DS implements edge_data  {
+    	private int src;
+    	private int dest;
+    	private String info;
+    	private int tag;
+    	private double weight;
+
+    		@Override
+    		public int getSrc() {
+    			// TODO Auto-generated method stub
+    			return this.src;
+    		}
+
+    		@Override
+    		public int getDest() {
+    			// TODO Auto-generated method stub
+    			return this.dest;
+    		}
+
+    		@Override
+    		public double getWeight() {
+    			// TODO Auto-generated method stub
+    			return this.weight;
+    		}
+
+    		@Override
+    		public String getInfo() {
+    			// TODO Auto-generated method stub
+    			return this.info;
+    		}
+
+    		@Override
+    		public void setInfo(String s) {
+    			this.info = s;
+    			
+    		}
+
+    		@Override
+    		public int getTag() {
+    			// TODO Auto-generated method stub
+    			return this.tag;
+    		}
+
+    		@Override
+    		public void setTag(int t) {
+    			this.tag=t;
+    			
+    		}
+
+    	}
 }
