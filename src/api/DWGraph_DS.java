c68458f39adefc0a7678c56d3f926b3b57dbe185
Reut-Maslansky <maslanskyr@gmail.com>
2020-11-22 19:35:51 +0200

    Node Data

--- src/api/DWGraph_DS.java
+++ src/api/DWGraph_DS.java
@@ -1,6 +1,9 @@
 package ex2.src.api;
 
+import ex1.src.node_info;
+
 import java.util.Collection;
+import java.util.HashMap;
 
 public class DWGraph_DS implements directed_weighted_graph {
     @Override
@@ -56,5 +59,158 @@ public class DWGraph_DS implements directed_weighted_graph {
     @Override
     public int getMC() {
         return 0;
+    }
+
+    private class Node_Data implements node_data {
+        private int idNode;
+        private double weight;
+        private geo_location geo;
+        private HashMap<Integer, node_info> neighbor;
+        private String info;
+        private int tag;
+
+
+        @Override
+        public int getKey() {
+            return this.idNode;
+        }
+
+        public Node_Data(int key, double w) {
+
+            this.idNode = key;
+            this.weight=w;
+            this.neighbor = new HashMap<>();
+            this.geo= new Geo_Location();
+            this.info = "";
+            this.tag = 0;
+        }
+
+        public Node_Data(node_data n) {
+            this.idNode = n.getKey();
+            this.neighbor = new HashMap<>();
+            this.info = n.getInfo() + "";
+            this.tag = n.getTag();
+            this.weight=n.getWeight();
+            this.geo= new Geo_Location(n.getLocation());
+        }
+
+        public Collection<node_info> getNi() {
+            return neighbor.values();
+        }
+
+        public void removeNei(node_info node) {
+            this.neighbor.remove(node.getKey());
+        }
+
+        @Override
+        public geo_location getLocation() {
+            return this.geo;
+        }
+
+        //////////////////////
+        @Override
+        public void setLocation(geo_location p) {
+
+        }
+
+        @Override
+        public double getWeight() {
+            return this.weight;
+        }
+
+        @Override
+        public void setWeight(double w) {
+            this.weight=w;
+        }
+
+        @Override
+        public String getInfo() {
+            return info;
+        }
+
+        @Override
+        public void setInfo(String s) {
+            this.info= s+"";
+        }
+
+        @Override
+        public int getTag() {
+            return this.tag;
+        }
+
+        @Override
+        public void setTag(int t) {
+            this.tag=t;
+        }
+
+        public String toString() {
+            return "" + idNode;
+        }
+
+        /**
+         * This method returns if this object and this node_info equals of their values.
+         *
+         * @param o
+         * @return
+         * */
+        @Override
+        public boolean equals(Object o){
+            if(o==null) return false;
+            if(!(o instanceof node_data)) return false;
+            int k= ((node_data)o).getKey();
+            if(getKey()!=k) return false;
+           /* if(getV(this.getKey())!=null && getV(k)!=null){
+                if(!(getV(this.getKey()).equals(getV(k)))) return false;
+            }
+            if(getTag()!=((node_info) o).getTag()) return false;
+            if(!(getInfo().equals(((node_info) o).getInfo()))) return false;
+            return true;
+        }*/
+
+        private class Geo_Location implements geo_location {
+            private double x;
+            private double y;
+            private double z;
+
+            public Geo_Location(){
+                this.x=0;
+                this.y=0;
+                this.z=0;
+            }
+
+            public Geo_Location(double x, double y, double z){
+                this.x=x;
+                this.y=y;
+                this.z=z;
+            }
+
+            public Geo_Location(geo_location g){
+                this.x=g.x();
+                this.y=g.y();
+                this.z=g.z();
+            }
+
+            @Override
+            public double x() {
+                return x;
+            }
+
+            @Override
+            public double y() {
+                return y;
+            }
+
+            @Override
+            public double z() {
+                return z;
+            }
+
+            //////////////////////////////
+            /////fix/////////
+            @Override
+            public double distance(geo_location g) {
+                return 0;
+            }
+        }
     }
 }
