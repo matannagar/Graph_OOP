fe72cffaac4bf7333cf221e930c6fde9dfcdfb18
matannagar <73398186+matannagar@users.noreply.github.com>
2020-12-28 17:28:58 +0200

    added tests

--- tests/test_DiGraph.py
+++ tests/test_DiGraph.py
@@ -3,19 +3,34 @@ import unittest
 from src.DiGraph import DiGraph
 
 
+def graph_creator() -> DiGraph:
+    graph = DiGraph()
+    for x in range(7):
+        graph.add_node(x)
+
+    graph.add_edge(1, 2, 2.5)
+    graph.add_edge(1, 3, 1.7)
+    graph.add_edge(2, 3, 0.3)
+    graph.add_edge(3, 2, 5.6)
+    graph.add_edge(5, 1, 3.2)
+    graph.add_edge(5, 0, 9.8)
+    graph.add_edge(0, 6, 3.8)
+
+    return graph
+
+
 class TestDiGraph(unittest.TestCase):
 
     def test_v_size(self):
-        graph = self.graph_creator()
+        graph = graph_creator()
         self.assertEqual(7, graph.v_size(), "The number of nodes is incorrect")
-
-        # self.assertEqual(True, graph.add_node(41), "Added a new node")
-        # self.assertEqual(False, graph.add_node(41), "No node was supposed to be added")
-        # self.assertEqual(False, graph.add_node(4), "No node was supposed to be added")
-        # add more tests
+        graph.add_node(41)
+        self.assertEqual(8, graph.v_size(), "Added a new node")
+        graph.add_node(41)
+        self.assertEqual(8, graph.v_size(), "No node was supposed to be added")
 
     def test_e_size(self):
-        graph = self.graph_creator()
+        graph = graph_creator()
         self.assertEqual(7, graph.e_size(), "The number of edges is incorrect")
         graph.add_edge(4, 0, 1)
         self.assertEqual(8, graph.e_size(), "Added an edge")
@@ -26,13 +41,12 @@ class TestDiGraph(unittest.TestCase):
         graph.add_edge(16, 4, 1)
         self.assertEqual(8, graph.e_size(), "No edges were supposed to be added")
         graph.add_edge(10, 16, 1)
-        self.assertEqual(8, graph.e_size() , "No edges were supposed to be added")
+        self.assertEqual(8, graph.e_size(), "No edges were supposed to be added")
         graph.add_edge(1, 1, 1)
         self.assertEqual(8, graph.e_size(), "No edges were supposed to be added")
 
-
     def test_get_mc(self):
-        graph = self.graph_creator()
+        graph = graph_creator()
         self.assertEqual(14, graph.get_mc(), "The number of MC is incorrect")
         graph.add_node(0)
         self.assertEqual(14, graph.get_mc(), "No change in MC after adding existing node")
@@ -50,7 +64,7 @@ class TestDiGraph(unittest.TestCase):
         self.assertEqual(22, graph.get_mc(), "The number of MC is incorrect: deleted node 1")
 
     def test_add_edge(self):
-        graph = self.graph_creator()
+        graph = graph_creator()
 
         self.assertEqual(True, graph.add_edge(4, 0, 1), "Added an edge")
         self.assertEqual(False, graph.add_edge(4, 0, 12), "No edges were supposed to be added")
@@ -60,21 +74,21 @@ class TestDiGraph(unittest.TestCase):
         self.assertEqual(False, graph.add_edge(1, 1, 1), "No edges were supposed to be added")
 
     def test_add_node(self):
-        graph = self.graph_creator()
+        graph = graph_creator()
 
         self.assertEqual(True, graph.add_node(41), "Added a new node")
         self.assertEqual(False, graph.add_node(41), "No node was supposed to be added")
         self.assertEqual(False, graph.add_node(4), "No node was supposed to be added")
 
     def test_remove_node(self):
-        graph = self.graph_creator()
+        graph = graph_creator()
 
         self.assertEqual(False, graph.remove_node(31), "No node was supposed to be removed")
         self.assertEqual(True, graph.remove_node(1), "Removed node 1")
         self.assertEqual(False, graph.remove_node(1), "Was already removed")
 
     def test_remove_edge(self):
-        graph = self.graph_creator()
+        graph = graph_creator()
 
         self.assertEqual(False, graph.remove_edge(4, 0), "No such edge")
         graph.add_edge(4, 0, 1)
@@ -84,18 +98,3 @@ class TestDiGraph(unittest.TestCase):
         self.assertEqual(True, graph.remove_edge(5, 1), "No edges were supposed to be removed")
         self.assertEqual(False, graph.remove_edge(10, 16), "No edges were supposed to be removed")
         self.assertEqual(False, graph.remove_edge(1, 1), "No edges were supposed to be removed")
-
-    def graph_creator(self) -> DiGraph:
-        graph = DiGraph()
-        for x in range(7):
-            graph.add_node(x)
-
-        graph.add_edge(1, 2, 2.5)
-        graph.add_edge(1, 3, 1.7)
-        graph.add_edge(2, 3, 0.3)
-        graph.add_edge(3, 2, 5.6)
-        graph.add_edge(5, 1, 3.2)
-        graph.add_edge(5, 0, 9.8)
-        graph.add_edge(0, 6, 3.8)
-
-        return graph
